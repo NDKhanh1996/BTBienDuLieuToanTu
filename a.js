083b1a6995bf4b0e2ace72b32d815e7aec9f541d
Nguyễn Duy Khánh <khanh@MacBook-Air-cua-Nguyen.local>
2023-03-08 17:08:23 +0700

    add .

---
+++ a.js
@@ -0,0 +1,10 @@
+Ly = prompt('Nhập điểm lý')
+Hoa = prompt('Nhập điểm hoá')
+Sinh = prompt('Nhập điểm sinh')
+let L= parseInt(Ly)
+let H = parseInt(Hoa)
+let S = parseInt(Sinh)
+T = L + H + S
+TB = T/3
+
+document.write("Điểm TB: " + TB)
